bd0869c8ab219bd82c14224e20b9ad7517d362a0
ElenaPalomar <elenapalomarllorente@gmail.com>
2022-05-17 10:35:41 +0200

    some changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const movies = require("./data/movies.json");
+const express = require('express');
+const cors = require('cors');
+// movies data
+const movies = require('./data/movies.json');
+
 // create and config server
 const server = express();
 server.use(cors());
@@ -13,31 +15,25 @@ server.listen(serverPort, () => {
 });
 
 
-// Creamos el endpoint para responder a la peticion que viene del font (getMoviesFromApi). 
-// La ruta es '/movies', como habíamos especificado en el fetch del front, pero esta ruta la hemos elegido/creado/configurado nosotras aquí, es decir, el fetch tiene que llamar a esta ruta que es la que aloja los datos que le pide al back, pero aquí es donde decidimos que esta es la ruta en la que se alojan. 
-// El verbo es GET, pues hacemos una petición de datos y no vamos a modificar nada en el servidor
+// api endpoint - quey params movies
 server.get('/movies', (req, res) => {
 
-  // Vemos que query params estamos recibiendo del front
-  console.log('Querys que recibimos del front:', req.query);
-
-  // guardamos el valor de las query params en una constante:
+  // query params
   const genderFilterParam = req.query.gender;
   const sortFilterParam = req.query.sort;
 
 
-  // Esta función sirve para ordenar las películas por orden alfabético. Esta función es un callBack para el método de arrays 'sort', que lo que hacer es comprobar la condición de los 'if' y evaluar el orden de los elementos en función del 'return'
-  const orderScenesAsc = (x, y) => { // La comparación se hace, de manera iterativa: x=0, y=1; x=0, y=2; ... x=1, y=0; x=1, y=2; ...
-    if (x.title < y.title) { // Si el valor de la función (el return) es < 0, entonces ordena 'x.title' antes que 'y.title'
+  // sort movies by title
+  const orderScenesAsc = (x, y) => {
+    if (x.title < y.title) {
       return -1;
-    } else if (x.title > y.title) { // Si el valor de la función (el return) es > 0, entonces ordena 'y.title' antes que 'x.title'
+    } else if (x.title > y.title) {
       return 1;
     } else {
-      return 0;  // Si el valor de la función (el return) es = 0, mantener el orden original de 'x.movie' y 'y.movie'
+      return 0;
     }
   };
 
-  // Esta función es la misma que la anterior pero en orden descendente
   const orderScenesDesc = (x, y) => { 
     if (x.title < y.title) { 
       return 1;
@@ -49,26 +45,24 @@ server.get('/movies', (req, res) => {
   };
 
 
-  // Para que funcionen los filtros en el navegador, es decir, para que al seleccionar el valor del select o del checkbox se filtren o se ordenen las películas, debemos realizar un 'filter' y un 'sort' (según el caso) y mandarle el valor de las películas filtradas a la respuesta del servidor para que éste a su vez se las mande al front que es el que las pinta
+  // filter and sort movies
   const filteredMovies = movies
     .filter(movie => {
       if(genderFilterParam === ''){
-        return true; // Le decimos que si está seleccionado el valor por defecto en el select (es decir, el valor que pone en el navegador 'Todas'), nos devuelva todas las películas
+        return true; 
       } else {
-        return movie.gender === genderFilterParam ? true : false; // Le decimos que si el género de cada película (movie.gender) es igual al parámetro que recibimos por query params 'genderFilterParam', entonces me devuelva esa película, la que cumple esa condición vamos
+        return movie.gender === genderFilterParam ? true : false; 
       }
     })
-    .sort(sortFilterParam === 'asc' ? orderScenesAsc : orderScenesDesc); // En este caso, la función 'sort' dice que si el checkbox tiene valor 'asc', las películas se ordenen alfabéticamente (como describe la función 'orderScenesAsc') y sino, que las ordene alfabéticamente inverso (como describe la función 'orderScenesDesc')
+    .sort(sortFilterParam === 'asc' ? orderScenesAsc : orderScenesDesc); 
 
 
-  // Esta es la variable que contiene la respuesta del servidor a la petición del front. En este caso, guardamos las variables que queremos que nos devuelva el servidor en la variable 'response'
+  // server response
   const response = {
     success: true,
-    movies: filteredMovies // Ponemos las películas almacenadas en './data/movies.json'
-     // Función para filtrar las 'movies' en función de los query params
-    
+    movies: filteredMovies
   }
 
-  // y luego le decimos que mande esa respuesta (almacenada en el parámetro 'res') en formato 'json' con los datos almacenador en esta variable (response)
+  // send server response in json format
   res.json(response);
 })
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,30 +1,15 @@
-// login
-
-/* En App.js vemos que, en el useEffect, la función 'getMoviesFromApi' recibe como parámetros el objeto:
-
- const params = {
-    gender: allMoviesOptionGender,
-    sort: allMoviesOptionSort
-  }; 
-
-  que a su vez recibe en la propiedad 'gender' por 'props' el valor del género seleccionado en el 'select' del front (del componente 'AllMovies'). Y, en la propiedad 'sort' recibe el valor clickado del checkbox del front que ordena las película alfabéticamente de A-Z o de Z-A.
-  */
-
 const getMoviesFromApi = (params) => {
   console.log('Se están pidiendo las películas de la app');
   console.log(params);
 
-  // Los parámetros que queremos enviar por query params son precisamente los que nos vienen del select por 'props' a la función 'getMoviesFromApi' y están en el objeto 'params', entonces los query params serán:
+  // create query params
   const queryParamsGender = `gender=${params.gender}`;
   const queryParamsSort = `sort=${params.sort}`;
-  
 
-  // La 'url' deonde se debe hacer la petición de datos es la de nuestro servidor '//localhost:4000'. Además, los datos están alojados en la ruta:'/movies'.
-  // A la 'url' anterior le añadimos los query params para filtrar por género y por orden alfabético
+
   return fetch(`//localhost:4000/movies?${queryParamsGender}&${queryParamsSort}`)
     .then(response => response.json())
     .then(data => {
-      // 'data' son los datos reales que retorna el servidor para pasárselos a React
       return data;
     });
 };
