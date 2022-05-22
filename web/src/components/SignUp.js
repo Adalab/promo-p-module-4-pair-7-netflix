ce932defca94fadccd270d756708189811275c96
ElenaPalomar <elenapalomarllorente@gmail.com>
2022-05-22 08:08:24 +0200

    add first extra (2password) and styles

--- web/src/components/SignUp.js
+++ web/src/components/SignUp.js
@@ -3,6 +3,12 @@ import React, { useState } from 'react';
 const SignUp = props => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState(false);
+
+/*   const regExpPassword = /^[0-9][A-Z][a-z]?.{8,}$/;
+  const correctPassword = regExpPassword.test(password);
+  console.log(correctPassword); */
 
   // events
 
@@ -14,16 +20,42 @@ const SignUp = props => {
     setPassword(ev.target.value);
   };
 
+  const handleConfirmPassword = ev => {
+    setConfirmPassword(ev.target.value);
+  };
+
   const handleForm = ev => {
     ev.preventDefault();
-    // Enviamos los datos a App y este al API
-    props.sendSingUpToApi({
-      email: email,
-      password: password
-    });
+
+    setPasswordErrorMessage(false);
+
+    if(password === confirmPassword){
+
+      // Enviamos los datos a App y este al API
+      props.sendSingUpToApi({
+        email: email,
+        password: password
+      });
+
+    } else {
+
+      setPasswordErrorMessage(true);
+
+    }
   };
 
   // render
+
+  const passwordErrorRender = () => {
+    // Si el API ha devuelto un error, APP lo guarda en el estado y nos lo pasa
+    if (passwordErrorMessage !== false) {
+      return (
+        <p className="errorMessage">
+          Error en el password: <span className="error">Las contraseñas no coinciden</span>
+        </p>
+      );
+    }
+  };
 
   const renderErrorMessage = () => {
     // Si el API ha devuelto un error, APP lo guarda en el estado y nos lo pasa
@@ -64,9 +96,22 @@ const SignUp = props => {
           onChange={handlePassword}
         />
 
+        <label className="formLogin__label" htmlFor="confirmPassword">
+          Vuelve a escribir tu contraseña
+        </label>
+        <input
+          className="formLogin__input"
+          type="text"
+          name="confirmPassword"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={handleConfirmPassword}
+        />
+
         <input className="button" type="submit" value="Registrar" />
 
         {renderErrorMessage()}
+        {passwordErrorRender()}
       </form>
     </section>
   );
